1239d5d0aaf1c52f59b94102440752b00d0873a7
Clement Carpot <clement.carpot@aquasys.fr>
2023-05-29 23:09:30 +0200

    edit fixture

--- migrations/Version20230529210530.php
+++ migrations/Version20230529210530.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230529203727 extends AbstractMigration
+final class Version20230529210530 extends AbstractMigration
 {
     public function getDescription(): string
     {
--- src/DataFixtures/AnnonceFixtures.php
+++ src/DataFixtures/AnnonceFixtures.php
@@ -20,7 +20,7 @@ class AnnonceFixtures extends Fixture
             $annonce->setPrix($faker->randomFloat(2, 10, 100));
             $annonce->setHeureDepart(new \DateTime($faker->time()));
             $annonce->setDateDepart($faker->dateTimeBetween('+1 week', '+2 weeks'));
-            $annonce->setVehicule($faker->randomElement(['Voiture', 'Moto']));
+            $annonce->setVehicule($faker->randomElement(['BMW Serie 3', 'AUDI R8', 'Renault Scenic', 'Peugeot 306']));
             // Définir d'autres propriétés de l'entité Annonce
 
             $manager->persist($annonce);
